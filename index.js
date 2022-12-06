41354458af2200581e932430e5ca4b889c46f05b
ItsFranklinMyDudes <86197341+ItsFranklinMyDudes@users.noreply.github.com>
2022-12-06 19:38:53 +1100

    Update index.js

--- index.js
+++ index.js
@@ -5,25 +5,10 @@ client.commands = new Collection();
 
 const fs = require('fs');
 const config = require('./config.json')
-//const SunRodAPI = require('sunrod-api');
-//client.sunrod = new SunRodAPI('z9MwcGD47wWn2mHu5nfprEpPYKC5TPJTk7hcHn8X46nyVQs9U7');
 
 const functions = fs.readdirSync("./functions").filter(file => file.endsWith(".js"));
 const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
 const commandFolders = fs.readdirSync("./commands");
-
-const express = require('express');
-const app = express();
-const port = 6969;
-
-app.get('/', function (req, res) {
-    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    const responseString = `Full URL is: ${fullUrl}`;
-    res.send(responseString)
-})
-app.listen(port, () => {
-    //console.log(`website is running from ${port}`) // Website is running
-});
 
 (async () => {
     for (file of functions) {
@@ -50,28 +35,3 @@ client.on('ready', async () => {
           });
     }, 5000)
 })
-
-
-/*
-const RPC = require('discord-rpc')
-const rpc = new RPC.Client({
-    transport: 'ipc'
-})
-
-rpc.on("ready", () => {
-    rpc.setActivity({
-        details: "Invite Franky Today",
-        state: "Join the Discord",
-        startTimestamp: new Date(),
-        largeImageKey: "large",
-        largeImageText: "Franklin",
-        buttons : [{label : "BOT" , url : "https://discord.com/api/oauth2/authorize?client_id=992309600361660466&permissions=8&scope=bot%20applications.commands"}, {label : "DISCORD", url : "https://discord.gg/pZwV7YXztt"}]
-    })
-
-    console.log("seccussfully turned on")
-})
-
-rpc.login({
-    clientId: '992309600361660466'
-})
-*/
