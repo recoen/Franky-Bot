2147bc82d4104c033fe9d3ed35e225ea2085a6f8
ItsFranklinMyDudes <86197341+ItsFranklinMyDudes@users.noreply.github.com>
2022-12-24 12:19:59 +1100

    Update ascii.js

---
+++ commands/fun/ascii.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require("discord.js");
+const figlet = require('figlet')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ascii')
+        .setDescription('convert text to ascii')
+        .addStringOption(option =>
+            option
+                .setName('text')
+                .setDescription('input text to convert')
+                .setRequired(true)
+                .setMaxLength(15)
+        ),
+
+    async execute(interaction) {
+        const text = interaction.options.getString('text')
+
+        figlet(`${text}`, function (err, data) {
+            if (err) {
+                console.log('Something went wrong...');
+                console.dir(err);
+                return;
+            }
+            interaction.reply(`\`\`\`${data}\`\`\``)
+        });
+    }
+}
