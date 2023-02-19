5b350a7314c4eb206c8739b80288eb0156ca65d9
ItsFranklinMyDudes <86197341+ItsFranklinMyDudes@users.noreply.github.com>
2023-02-19 20:14:14 +1100

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder, EmbedBuilder, version } = require("discord.js");
 const moment = require('moment')
 require("moment-duration-format");
+const os = require('os')
+const config = require('../../config.json')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -20,36 +22,12 @@ module.exports = {
             .setDescription(`<@${client.user.id}> is a fun bot that has looks of commands, the main porpuse of this bot, is to have fun trolling friends and just have fun. <@${client.user.id}> has so many different features, Music, econmy and more`)
             .setThumbnail(client.user.displayAvatarURL())
             .addFields(
-                {
-                    name: `Total Commands`,
-                    value: `${command}`,
-                    inline: true
-                },
-                {
-                    name: `Total Servers`,
-                    value: `${servers}`,
-                    inline: true
-                },
-                {
-                    name: `Bot Ping`,
-                    value: `${ping}ms`,
-                    inline: true
-                },
-                {
-                    name: `Discord.js Version`,
-                    value: `v${version}`,
-                    inline: true
-                },
-                {
-                    name: `Node Version`,
-                    value: `${process.version}`,
-                    inline: true
-                },
-                {
-                    name: `Uptime`,
-                    value: `${duration}`,
-                    inline: true
-                }
+                { name: `Total Commands`, value: `${command}`, inline: true },
+                { name: `Total Servers`, value: `${servers}`, inline: true},
+                { name: `Bot Ping`, value: `${ping}ms`, inline: true },
+                { name: `Uptime`, value: `${duration}`, inline: true },
+                { name: `Discord Invite`, value: `[Here](${config.discord.invite})`, inline: true},
+                { name: `Bot Invite`, value: `[Here](https://discord.com/api/oauth2/authorize?client_id=${config.discord.clientId}&permissions=1789394406464&scope=bot%20applications.commands)`, inline: true}
             )
             .setFooter({ text: `Franklin#8332 Coded this bot`, iconURL: 'https://images-ext-1.discordapp.net/external/0edtCJmJ4eJeZKAQ9xR19RXeO596fTryBtEiGTNufKA/%3Fsize%3D512/https/cdn.discordapp.com/avatars/692197998939209789/18279117d2ebc55a4ddd8147da9137a5.webp'})
 
