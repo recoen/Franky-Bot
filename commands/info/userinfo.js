55b1fb3cc2eaedf850a6c0411c3b296ebd9151af
ItsFranklinMyDudes <86197341+ItsFranklinMyDudes@users.noreply.github.com>
2023-02-19 20:13:50 +1100

    Update userinfo.js

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const moment = require('moment')
+const em = require('../../emoji.json')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,22 +11,39 @@ module.exports = {
                 .setName('target')
                 .setDescription('Target to get info on this user')
         ),
-        category: 'Info',
-        cooldown: 0,
+    category: 'Info',
+    cooldown: 0,
     async execute(interaction) {
         let target = interaction.options.getUser('target') || interaction.user;
         let member = interaction.guild.members.cache.get(target.id)
+        let flags = target.flags.toArray();
 
         const embed = new EmbedBuilder()
-            .setTitle(`${target.username}`)
+            .setTitle(`Info About ${target.username}`)
             .setThumbnail(target.displayAvatarURL({ dynamic: true }))
             .setDescription(`
-            User ID: ${target.id}
-            Roles: ${member.roles.cache.map(r => r).join(" ").replace("@everyone", "ㅤ")}
-            Discord User Since: ${moment(target.createdAt).format('MMMM Do YYYY, h:mm:ss a')}\n**-** ${moment(target.createdAt).startOf('day').fromNow()}
-            Server Member Since: ${moment(member.joinedAt).format('MMMM Do YYYY, h:mm:ss a')}\n**-** ${moment(member.joinedAt).startOf('day').fromNow()}
+            **Name:** ${target.username}
+            **Discriminator:** ${target.discriminator}
+            **ID:** ${target.id}
+            **Badges:** ${flags.join(' ')
+                    .replace('HypeSquadOnlineHouse1', em.badges.bravery)
+                    .replace('HypeSquadOnlineHouse2', em.badges.brillance)
+                    .replace('HypeSquadOnlineHouse3', em.badges.balance)
+                    .replace('ActiveDeveloper', em.badges.activeDeveloper)
+                    .replace('Staff', em.badges.staff)
+                    .replace('Partner', em.badges.partner)
+                    .replace('HypeSquad', em.badges.hypeSquad)
+                    .replace('PremiumEarlySupporter',em.badges.premiumEarlySupporter)
+                    .replace('BugHunterLevel1', em.badges.BugHunter1)
+                    .replace('BugHunterLevel2', em.badges.BugHunter2)
+                    .replace('VerifiedBot', em.badges.verifiedBot)
+                    .replace('CertifiedModerator', em.badges.certifiedMod)
+                || 'null'}
+            **Discord User Since:** ${moment(target.createdAt).format('MMMM Do YYYY')} - ${moment(target.createdAt).startOf('day').fromNow()}
+            **Server Member Since:** ${moment(member.joinedAt).format('MMMM Do YYYY')} - ${moment(member.joinedAt).startOf('day').fromNow()}
         `)
             .setColor('#2f3136')
+            .setTimestamp()
 
         interaction.reply({ embeds: [embed] })
     }
