7412f9cb395b927dad058a0e38a04f51f2767e1c
ItsFranklinMyDudes <86197341+ItsFranklinMyDudes@users.noreply.github.com>
2023-03-27 16:25:20 +1100

    Delete schema directory

--- schema/economySchema.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema } = require('mongoose')
-
-let economyschema = new Schema({
-    User: String,
-    Money: Number,
-}, { versionKey: false } )
-
-module.exports = model('money', economyschema)
