0427a4d30cfaa64127b20e0d66181e5c19818c6a
ItsFranklinMyDudes <86197341+ItsFranklinMyDudes@users.noreply.github.com>
2023-04-05 03:20:03 -0700

    Add files via upload

---
+++ src/functions/handelCommands.js
@@ -0,0 +1,36 @@
+const { REST } = require("@discordjs/rest");
+const { Routes } = require('discord-api-types/v9');
+const fs = require('fs');
+const config = require('../config.json')
+require('colors')
+
+module.exports = (client) => {
+    client.handleCommands = async (commandFolders, path) => {
+        client.commandArray = [];
+        for (folder of commandFolders) {
+            const commandFiles = fs.readdirSync(`${path}/${folder}`).filter(file => file.endsWith('.js'));
+            for (const file of commandFiles) {
+                const command = require(`../commands/${folder}/${file}`);
+                client.commands.set(command.data.name, command);
+                client.commandArray.push(command.data.toJSON());
+                console.log('[COMMANDS] '.grey + `${command.data.name}`.bold + ' was Loaded'.green)
+            }
+        }
+        console.log(' ')
+        const rest = new REST({
+            version: '9'
+        }).setToken(config.discord.token);
+
+        (async () => {
+            try {
+                await rest.put(
+                    Routes.applicationCommands(config.discord.clientId), {
+                        body: client.commandArray
+                    },
+                );
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    };
+};
